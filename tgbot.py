20d1a46cd39ed601a85ffa859b473de35fd3a13e
ravi507xx-netizen <ravi507xx@gmail.com>
2025-11-12 09:56:45 +0530

    Add check for empty message in send_message_to_admin

--- tgbot.py
+++ tgbot.py
@@ -3,6 +3,8 @@ import requests
 def send_message_to_admin(bot_token, admin_id, message):
     """Send message to Telegram Admin"""
     try:
+        if not message:
+            return False
         url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
         data = {"chat_id": admin_id, "text": message}
         response = requests.post(url, data=data)
